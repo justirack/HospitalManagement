ab24f64ac60754949c3a1af2dbb855ba7b7577ea
Justin Rackley <justinrackleyy@gmail.com>
2021-04-19 12:04:26 -0300

    update patient.java
    setup constructors, getters and setters in patient.java

--- src/main/java/com/example/demo/patient/Patient.java
+++ src/main/java/com/example/demo/patient/Patient.java
@@ -1,11 +1,9 @@
 package com.example.demo.patient;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
 import javax.persistence.*;
 
 @Entity(name = "patient")
-@Table(name = "patient")
+@Table
 public class Patient {
 
     //A persons ssn is the primary key
@@ -26,4 +24,61 @@ public class Patient {
     private Long phone;
     @Column(name = "address",nullable = false,columnDefinition = "TEXT")
     private String address;
+
+    public Patient(Long ssn, String familyDoctor, String firstName, String lastName, Long phone, String address) {
+        this.ssn = ssn;
+        this.familyDoctor = familyDoctor;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public Patient() {
+    }
+
+
+    public void setFamilyDoctor(String familyDoctor) {
+        this.familyDoctor = familyDoctor;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getSsn() {
+        return ssn;
+    }
+
+    public String getFamilyDoctor() {
+        return familyDoctor;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
