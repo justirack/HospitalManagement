f220b88a2fcfcce3f8e85a929614b8e00fa4170b
Justin Rackley <justinrackleyy@gmail.com>
2021-04-19 17:09:25 -0300

    updated PatientController
    added more methods to PatientController

--- src/main/java/com/example/demo/patient/PatientController.java
+++ src/main/java/com/example/demo/patient/PatientController.java
@@ -1,9 +1,14 @@
+/**
+ * create a class to help serve REST enpoints
+ * and perform CRUD operations
+ * @author - Justin Rackley
+ */
+
 package com.example.demo.patient;
 
+import com.example.demo.doctor.Doctor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +31,28 @@ public class PatientController {
         return patientService.getPatients();
     }
 
+    @PostMapping
+    public void addNewPatient(@RequestBody Patient patient){
+        patientService.addNewPatient(patient);
+    }
+
+    @DeleteMapping(path = "patientSsn")
+    public void deletePatient(@PathVariable("patientSsn")Long ssn){
+        patientService.removePatientBySsn(ssn);
+    }
+
+    @PutMapping(path = "patientSsn")
+    public void updatePatient(@PathVariable("patientSsn") Long ssn,
+                              @RequestParam(required = false) String firstName,
+                              @RequestParam(required = false) String lastName,
+                              @RequestParam(required = false) Doctor familyDoctor,
+                              @RequestParam(required = false) Integer phone,
+                              @RequestParam(required = false) String address){
+        patientService.changeName(ssn,firstName,lastName);
+        patientService.changeAddress(ssn,address);
+        patientService.changeFamilyDoctor(ssn,familyDoctor);
+        patientService.changePhone(ssn, phone);
+    }
 
 
 }
