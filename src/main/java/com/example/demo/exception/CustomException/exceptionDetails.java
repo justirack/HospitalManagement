698a5af7388dfe09da7720b14c68bbb848e0f419
Justin Rackley <justinrackleyy@gmail.com>
2021-06-03 15:05:13 -0300

    Added status code to custom error message
    Added a status code to the  invalidIdException handler

--- src/main/java/com/example/demo/exception/CustomException/exceptionDetails.java
+++ src/main/java/com/example/demo/exception/CustomException/exceptionDetails.java
@@ -1,17 +1,19 @@
 package com.example.demo.exception.CustomException;
 
+import org.springframework.http.HttpStatus;
+
 public class exceptionDetails {
 
     private final String message;
     private final String details;
+    private final int statusCode;
 
-    public exceptionDetails(String message, String details){
+    public exceptionDetails(String message, String details, int statusCode){
         super();
         this.message = message;
         this.details = details;
+        this.statusCode = statusCode;
     }
-
-
 
     public String getMessage() {
         return message;
@@ -19,5 +21,9 @@ public class exceptionDetails {
 
     public String getDetails() {
         return details;
+    }
+
+    public int getStatus() {
+        return statusCode;
     }
 }
--- src/main/java/com/example/demo/exception/ExceptionHandling/GlobalHandler.java
+++ src/main/java/com/example/demo/exception/ExceptionHandling/GlobalHandler.java
@@ -5,22 +5,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import com.example.demo.exception.CustomException.exceptionDetails;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(InvalidIdException.class)
-    public final ResponseEntity<Object> handleInvalidId(InvalidIdException exception, WebRequest request){
+    public final ResponseEntity<Object> handleInvalidId(InvalidIdException exception){
         String details = exception.getLocalizedMessage();
         String message = "The ID you entered was invalid. Please enter a valid id and try again";
-        exceptionDetails exceptionDetails = new exceptionDetails(message,details);
-        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
+        final int statusCode = HttpStatus.NOT_FOUND.value();
+        exceptionDetails exceptionDetails = new exceptionDetails(message,details, statusCode);
+        return new ResponseEntity<>(exceptionDetails,HttpStatus.BAD_REQUEST);
     }
 
 }
