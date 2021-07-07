c65c810da9714a657a817c0786d09f24d43ad761
Justin Rackley <justinrackleyy@gmail.com>
2021-07-07 11:45:26 -0300

    Finish re working AppointmentController and update documentation.

--- src/main/java/com/hospital/manager/appointment/AppointmentController.java
+++ src/main/java/com/hospital/manager/appointment/AppointmentController.java
@@ -3,21 +3,9 @@
 */
 package com.hospital.manager.appointment;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import com.hospital.manager.exception.CustomException.AppointmentNotFoundException;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModel;
@@ -25,12 +13,26 @@ import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * <p>
@@ -46,7 +48,9 @@ import lombok.extern.slf4j.Slf4j;
 public final class AppointmentController
 {
     /**
-     * Allow a user to get a list of all appointments.
+     * <p>
+     *     Allow a client to get a list of all {@link Appointment}.
+     * </p>>
      * @return The list of all appointments.
      */
     @GetMapping
@@ -102,16 +106,16 @@ public final class AppointmentController
 
         // No appointment was found, return an empty list for now.
         // TODO: we should return a 404 Not Found in this case.
-        return Collections.emptyList(); 
+        throw new AppointmentNotFoundException("Unable to load appointment(s)");
     }
 
     /**
-     * Allow a user to book an appointment.
-     *
-     * @param patientSsn The ssn of the patient booking the appointment.
-     * @param doctorEmpId The empId of the doctor the appointment is being booked with.
-     * @param date The date of the appointment.
-     * @param room The room the appointment is in.
+     * <p>
+     *     Allow a client to book an {@link Appointment}.
+     * </p>
+     * @param payload The payload containing the {@link com.hospital.manager.patient.Patient} and
+     *                {@link com.hospital.manager.doctor.Doctor} information for the new appointment.
+     * @return The status code indicating if the appointment was successfully booked.
      */
     @PostMapping
     public HttpStatus create(final CreateRequestPayload payload)
@@ -119,40 +123,47 @@ public final class AppointmentController
         return service.book(
             payload.getSsn(),
             payload.getDoctorId(),
-            // FIXME - The incoming payload date string needs to be converted into
-            //         its equivalent Date object here.
-            new Date(),
+            FORMATTER.parse(payload.getDate()),
             payload.getRoom());
     }
 
     /**
-     * Allow a user to change the date of an appointment.
-     * @param appId The id of the appointment.
-     * @param date The new date of the appointment.
+     * <p>
+     *     Allow a client to cancel an {@link Appointment}.
+     * </p>
+     * @param payload The payload containing the id of the appointment to cancel.
+     * @return The status of if the appointment was canceled successfully.
      */
-    @PutMapping("changeDate/{appId}/{date}")
-    public HttpStatus changeDate(@PathVariable final long appId, @PathVariable final Date date){
-        return service.changeDate(appId, date);
+    @DeleteMapping(path = "cancel")
+    public HttpStatus cancel(final DeleteRequestPayload payload){
+        return service.cancel(payload.id);
     }
 
     /**
-     * Allow a user to cancel an appointment.
-     * @param appId The id of the appointment to cancel.
+     * <p>
+     *    Allow a client to change the date of an {@link Appointment}.
+     * </p>>
+     * @param payload The payload containing the appointment's if and its new date.
+     * @return The status of if the appointment's date was changed successfully.
      */
-    @DeleteMapping(path = "cancel/{appId}")
-    public HttpStatus cancel(@PathVariable final long appId){
-        return service.cancel(appId);
+    @PutMapping("changeDate")
+    public HttpStatus changeDate(final UpdateRequestPayload payload){
+        return service.changeDate(payload.getId(), FORMATTER.parse(payload.getDate()));
     }
 
     /**
-     * Allow a user to change the room of an appointment.
-     * @param appId The id of the appointment.
-     * @param room The new room of the appointment.
+     * <p>
+     *     Allow a client to change the date of an {@link Appointment}.
+     * </p>>
+     * @param payload The payload containing the appointment's if and its new room number.
+     * @return The status of if the appointment's room was changed successfully.
      */
-    @PutMapping("changeRoom/{appId}/{room}")
-    public HttpStatus changeRoom(@PathVariable final long appId, @PathVariable final int room){
-        return service.changeRoom(appId, room);
+    @PutMapping("changeRoom")
+    public HttpStatus changeRoom(final UpdateRequestPayload payload){
+        return service.changeRoom(payload.getId(),payload.getRoom());
     }
+
+
 
     @Getter
     @ToString
@@ -182,6 +193,20 @@ public final class AppointmentController
         private final long doctorId;
         private final String date;
         private final int room;
+    }
+
+    @Getter
+    @ToString
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @NoArgsConstructor(access = AccessLevel.PACKAGE)
+    @ApiModel(description = "The request details supplied when deleting an appointment")
+    public static final class DeleteRequestPayload{
+        @ApiModelProperty(
+                value = "The unique, database identifier for the appointment to delete. This cannot be null",
+                required = true,
+                example = "1024")
+        private Long id;
+
     }
 
     @ToString
@@ -219,4 +244,11 @@ public final class AppointmentController
     }
 
     private final AppointmentService service;
+    private final Appointment appointment;
+
+    // A date formatter used to produce string representations for the date objects
+    // contained within this class. This formatter captures the date and time (
+    // to the minute only). This does not take into account timezones.
+    private static final SimpleDateFormat FORMATTER
+            = new SimpleDateFormat("yyyy-MM-dd hh:mm");
 }
