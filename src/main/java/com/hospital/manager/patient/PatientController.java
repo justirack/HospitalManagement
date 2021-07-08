818f2bef866bbb102f4f38ab1437943de2377b86
Justin Rackley <justinrackleyy@gmail.com>
2021-07-08 17:24:58 -0300

    Complete re-work of Controller class.

--- src/main/java/com/hospital/manager/patient/PatientController.java
+++ src/main/java/com/hospital/manager/patient/PatientController.java
@@ -3,10 +3,24 @@
 */
 package com.hospital.manager.patient;
 
-import java.util.Collections;
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.hospital.manager.appointment.Appointment;
+import com.hospital.manager.doctor.Doctor;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.hospital.manager.exception.CustomException.FailedRequestException;
+import com.hospital.manager.exception.CustomException.NotFoundException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,125 +31,182 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hospital.manager.doctor.DoctorService;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
- * Create a class to help serve REST endpoints and perform CRUD operations.
- * This is the "API layer" that a user will interact with.
- * This class should be accessible by: 1.Management 2.Patients(update method only).
- * @author - Justin Rackley
+ * <p>
+ *     Represents the REST endpoints that provide CRUD functionality for the
+ *     underlying {@link Patient} objects by this service.
+ * </p>
  */
+@Slf4j
+@RequiredArgsConstructor
 @RestController
+@Api(tags = "patient")
 @RequestMapping(path = "patient")
 public final class PatientController {
 
-    //create a permanent reference to patient service
-    private final PatientService service;
-    private final DoctorService doctorService;
+    @GetMapping(path = "get")
+    @ApiOperation("Retrieves a single patient or a list of patients")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "The patient(s) were retrieved successfully"),
+            @ApiResponse(code = 404,message = "If no patient(s) correspond to the criteria supplied")
+    })
+    public List<PatientResponsePayload> get (RetrievalRequestPayload payload){
+        log.info("Attempting to find patient pertaining to request={}",payload.getId());
 
-    //inject patientService's bean into this class' bean
-    @Autowired
-    public PatientController(final PatientService service, final DoctorService doctorService) {
-        this.service = service;
-        this.doctorService = doctorService;
+        if (payload.getId() == null){
+            final List<PatientResponsePayload> results = new ArrayList<>();
+            final List<Patient> patients = service.getPatients();
+
+            log.info("Found {} results, returning them",patients.size());
+
+            for (Patient patient:patients) {
+                results.add(new PatientResponsePayload(patient));
+            }
+            return results;
+        }
+        final Patient patient = service.getPatient(payload.getId());
+
+        if (patient != null){
+            log.info("returning patient={}",patient);
+
+            final PatientResponsePayload results = new PatientResponsePayload(patient);
+            return Collections.unmodifiableList(List.of(results));
+        }
+        throw new NotFoundException("The patient you requested could not be found. Please Make sure all" +
+                " information is correct and try again");
     }
 
+    @PutMapping(path = "update")
+    public PatientResponsePayload update (final UpdateRequestPayload payload){
+        HttpStatus status = HttpStatus.NOT_FOUND;
 
-    /**
-     * Allow a user to get a list of all patients in the database.
-     * @return A list of all patients in teh database.
-     */
-    @GetMapping(path = "getPatients")
-    public List<Patient> getPatients(){
-        //make the returned collection unmodifiable
-        return Collections.unmodifiableList(service.getPatients());
-    }
-
-    /**
-     * Allow a user to get a single patient from the database.
-     * @param ssn The ssn of the patient.
-     * @return The patient.
-     */
-    @GetMapping(path = "getPatient/{ssn}")
-    public Patient getPatient(@PathVariable final long ssn){
-        return service.getPatient(ssn);
-    }
-
-    /**
-     * Allow a user to add a new patient to the database.
-     */
-    @PostMapping(path = "add")
-    public void addPatient(@RequestParam final long doctorId,
-                           @RequestParam final String firstName,
-                           @RequestParam final String lastName,
-                           @RequestParam final String phone,
-                           @RequestParam final String address){
-        //make sure doctor's id exists, throws exception if not
-        if (doctorService.getDoctor(doctorId) != null){
-            service.add(doctorId,firstName,lastName,phone,address);
+        if (payload.getFirstName() != null){
+            status = service.changeFirstName(payload.getId(), payload.getFirstName());
+        }
+        if (payload.getLastName() != null){
+            status = service.changeLastName(payload.getId(), payload.getLastName());
+        }
+        if (payload.getPhone() != null){
+            status = service.changePhone(payload.getId(), payload.getPhone());
+        }
+        if (payload.getAddress() != null){
+            status = service.changeAddress(payload.getId(), payload.getAddress());
+        }
+        if (payload.getDoctorId() != null) {
+            status = service.changeFamilyDoctor(payload.getId(), payload.getDoctorId());
         }
 
+        if (status.equals(HttpStatus.OK)){
+            Patient patient = service.getPatient(payload.getId());
+            return new PatientResponsePayload(patient);
+
+        }
+        throw new FailedRequestException("One or more pieces of information could not be updated. " +
+                "Please make sure all information is correct and try again.");
     }
 
-    /**
-     * Allow a user to delete a person by their ssn.
-     * @param ssn The person to deletes ssn.
-     */
-    @DeleteMapping(path = "delete/{ssn}")
-    public HttpStatus deletePatient(@PathVariable final long ssn){
-        return service.remove(ssn);
+    @PostMapping(path = "add")
+    public HttpStatus add(final CreateRequestPayload payload){
+        return service.add(
+                payload.getDocId(),
+                payload.getFirstName(),
+                payload.getLastName(),
+                payload.getPhone(),
+                payload.getAddress()
+        );
     }
 
-    /**
-     * Allow a user to change their first name.
-     * @param ssn The patients ssn.
-     * @param firstName The patients new first name.
-     */
-    @PutMapping(path = "changeFirstName/{ssn}/{firstName}")
-    public HttpStatus changeFirstName(@PathVariable final long ssn, @PathVariable final String firstName){
-        return service.changeFirstName(ssn,firstName);
-    }
-
-    /**
-     * Allow a user to change their first name.
-     * @param ssn The patients ssn.
-     * @param lastName The patients new last name
-     */
-    @PutMapping(path = "changeLastName/{ssn}/{lastName}")
-    public HttpStatus changeLastName(@PathVariable final long ssn, @PathVariable final String lastName){
-        return service.changeLastName(ssn,lastName);
-    }
-
-    /**
-     * Allow a user to change their address.
-     * @param ssn The patients ssn.
-     * @param address The patients new address.
-     */
-    @PutMapping(path = "changeAddress/{ssn}/{address}")
-    public HttpStatus changeAddress(@PathVariable final long ssn, @PathVariable final String address){
-        return service.changeAddress(ssn,address);
-    }
-
-    /**
-     * Allow a user to change their family doctor.
-     * @param ssn The patients ssn.
-     * @param familyDocId The patients new family doctor's id.
-     */
-    @PutMapping(path = "changeDoctor/{ssn}/{familyDocId}")
-    public HttpStatus changeFamilyDoctor(@PathVariable final long ssn, @PathVariable final long familyDocId){
-        return service.changeFamilyDoctor(ssn,familyDocId);
-    }
-
-    /**
-     * Allow a user to change their phone number.
-     * @param ssn The patients ssn.
-     * @param phone The users new phone number.
-     */
-    @PutMapping(path = "changePhone/{ssn}/{phone}")
-    public HttpStatus changePhone(@PathVariable final long ssn, @PathVariable final String phone){
-        return service.changePhone(ssn,phone);
+    @DeleteMapping(path = "delete")
+    public HttpStatus remove(final DeleteRequestPayload payload){
+        return service.remove(payload.getId());
     }
 
 
+
+    @ToString
+    @Getter
+    @RequiredArgsConstructor
+    @ApiModel(description = "The request details supplied when adding a new patient to the database.")
+    private static final class CreateRequestPayload{
+        private final String firstName;
+        private final String lastName;
+        private final String phone;
+        private final String address;
+        private final Long docId;
+    }
+
+    @ToString
+    @Getter
+    @RequiredArgsConstructor
+    @ApiModel(description = "The request details supplied when retrieving a patient from the database.")
+    private static final class RetrievalRequestPayload{
+        @ApiModelProperty(
+                value = "The unique, database identifier for the patient to retrieve" +
+                        " If null, return all patients in the database",
+                required = true,
+                example = "1024")
+        private final Long id;
+    }
+
+    @ToString
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @RequiredArgsConstructor
+    @ApiModel(description = "The request details supplied when updating a patients information.")
+    private static final class UpdateRequestPayload{
+        @ApiModelProperty(value = "The patient's id",required = true)
+        final Long id;
+        @ApiModelProperty(value = "The patient's new first name")
+        final String firstName;
+        @ApiModelProperty(value = "The patient's new last name")
+        final String lastName;
+        @ApiModelProperty(value = "The patient's new phone number")
+        final String phone;
+        @ApiModelProperty(value = "The patient's new address")
+        final String address;
+        @ApiModelProperty(value = "The patient's new doctor's id")
+        final Long doctorId;
+    }
+
+    @ToString
+    @Getter
+    @RequiredArgsConstructor
+    @ApiModel(description = "The request details supplied when deleting a patient from the database.")
+    private static final class DeleteRequestPayload{
+        @ApiModelProperty(
+                value = "The unique, database identifier for the patient to retrieve" +
+                        " If null, return all patients in the database",
+                required = true,
+                example = "1024")
+        private final Long id;
+    }
+
+    @ToString
+    @Getter
+    private static final class PatientResponsePayload{
+        public PatientResponsePayload(final Patient patient) {
+            id = patient.getId();
+            firstName = patient.getFirstName();
+            lastName = patient.getLastName();
+            phone = patient.getPhone();
+            address = patient.getAddress();
+            doctor = patient.getDoctor();
+            appointments = patient.getAppointments();
+        }
+
+        private final Long id;
+        private final String firstName;
+        private final String lastName;
+        private final String phone;
+        private final String address;
+        private final Doctor doctor;
+        private final List<Appointment> appointments;
+    }
+
+    private final PatientService service;
 
 }
