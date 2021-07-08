d38fec8ef943fb75cea84467a1cc1cc96497e5dd
Justin Rackley <justinrackleyy@gmail.com>
2021-07-08 17:24:08 -0300

    Add ability to search a patient by their phone number.

--- src/main/java/com/hospital/manager/patient/PatientRepository.java
+++ src/main/java/com/hospital/manager/patient/PatientRepository.java
@@ -3,10 +3,10 @@
 */
 package com.hospital.manager.patient;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 /**
  * This interface will allow access to the database.
@@ -14,4 +14,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface PatientRepository extends JpaRepository<Patient,Long> {
     @Query("SELECT s FROM Patient s WHERE s.id = ?1")
     Optional<Patient>findPatientBySsn(final long id);
+
+    @Query("SELECT s FROM Patient s WHERE s.phone = ?1")
+    Optional<Patient>findPatientByPhone(final String phone);
 }
