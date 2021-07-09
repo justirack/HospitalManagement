f68b8ad7451485394a3cb7e539cddbb1ac952041
Justin Rackley <justinrackleyy@gmail.com>
2021-07-09 19:52:34 -0300

    Complete re work of controller.

--- src/main/java/com/hospital/manager/prescription/PrescriptionController.java
+++ src/main/java/com/hospital/manager/prescription/PrescriptionController.java
@@ -4,81 +4,207 @@
 package com.hospital.manager.prescription;
 
 
-import java.util.Collections;
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.hospital.manager.doctor.Doctor;
+import com.hospital.manager.exception.CustomException.FailedRequestException;
+import com.hospital.manager.patient.Patient;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Create a class to help serve REST endpoints and perform CRUD operations.
- * This is the "API layer" that a user will interact with.
- * This class should only be accessible by: 1.Doctors.
- * @author - Justin Rackley
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+@Slf4j
+@RequiredArgsConstructor
 @RestController
+@Api(tags = "prescription")
 @RequestMapping(path = "prescription")
 public final class PrescriptionController {
-    //create a permanent reference to prescriptionService
+
+    /**
+     * <p>
+     *     Allow a client to get a patient or a list of prescription from the database.
+     * </p>
+     * @param payload THe payload containing the prescriptions information.
+     * @return The prescription or a list of prescriptions.
+     */
+    @GetMapping(path = "get")
+    @ApiOperation("Retrieves a single prescription or a list of prescriptions")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "If the prescription(s) were retrieved successfully"),
+            @ApiResponse(code = 404, message = "If no prescription(s) correspond to the criteria supplied")
+    })
+    public List<PrescriptionResponsePayload> get(final RetrievalRequestPayload payload){
+        log.info("Attempting to find prescription pertaining to request={}", payload);
+
+        if (payload.getId() == null){
+            final List<PrescriptionResponsePayload> results = new ArrayList<>();
+            final List<Prescription> prescriptions = service.getPrescriptions();
+            log.info("found {} results. Returning them",prescriptions.size());
+
+            for (Prescription prescription : prescriptions) {
+                results.add(new PrescriptionResponsePayload(prescription));
+            }
+            return results;
+        }
+
+        Prescription prescription = service.getPrescription(payload.getId());
+
+        if (prescription != null){
+            log.info("returning prescription {}", prescription);
+
+            final PrescriptionResponsePayload results = new PrescriptionResponsePayload(prescription);
+            return Collections.unmodifiableList(List.of(results));
+        }
+        throw new FailedRequestException("Unable to load prescription(s). Please make sure all information " +
+                "is correct and try again");
+    }
+
+    /**
+     * <p>
+     *     Allow a client to update a prescription information.
+     * </p>
+     * @param payload the payload containing the prescriptions new information.
+     * @return The prescriptions new information.
+     */
+    @PutMapping(path = "update")
+    @ApiOperation("Updates a prescription information")
+    public PrescriptionResponsePayload update(final UpdateRequestPayload payload){
+        log.info("updating information for prescription {}",payload);
+
+        if (payload.getName() != null){
+            log.info("Updating the name of prescription with id {}",payload.getId());
+            service.changeName(payload.getId(),payload.getName());
+        }
+        if (payload.getAmount() != null){
+            log.info("Updating the amount of prescription with id {}",payload.getId());
+            service.changeAmount(payload.getId(),payload.getAmount());
+        }
+        Prescription prescription = service.getPrescription(payload.getId());
+        return new PrescriptionResponsePayload(prescription);
+    }
+
+    /**
+     * <p>
+     *     Allow a client to add a prescription to the database.
+     * </p>
+     * @param payload The payload containing the prescriptions information.
+     * @return The status of if the prescription was successfully added.
+     */
+    @PutMapping(path = "add")
+    @ApiOperation("Adds a prescription to the database")
+    public HttpStatus add (final CreatePrescriptionPayload payload){
+        return service.add(payload.getName(),payload.getPatient(),payload.getDoctor(),payload.getAmount());
+    }
+
+    /**
+     * <p>
+     *     Allow a client to delete a prescription from the database.
+     * </p>
+     * @param payload The payload containing the prescriptions id.
+     * @return The status of if the prescription was successfully deleted.
+     */
+    @DeleteMapping(path = "delete")
+    @ApiOperation("Removes a prescription from the database")
+    public HttpStatus delete(final  DeleteRequestPayload payload){
+        return service.delete(payload.getId());
+    }
+
+
+
+
+    @ToString
+    @Getter
+    @RequiredArgsConstructor
+    @ApiModel(description = "The request details supplied when adding a new prescription to the database.")
+    private static final class CreatePrescriptionPayload{
+        @ApiModelProperty(value = "The prescriptions name.")
+        final String name;
+        @ApiModelProperty(value = "The prescriptions amount")
+        final Long amount;
+        @ApiModelProperty("The prescriptions patient.")
+        final Patient patient;
+        @ApiModelProperty("The prescriptions doctor.")
+        final Doctor doctor;
+    }
+
+    @ToString
+    @Getter
+    @RequiredArgsConstructor
+    @ApiModel(description = "The request details supplied when retrieving a prescription from the database.")
+    private static final class RetrievalRequestPayload{
+        @ApiModelProperty(
+                value = "The unique, database identifier for the prescription to retrieve" +
+                        " If null, return all prescriptions in the database.",
+                required = true,
+                example = "1024"
+        )
+        final Long id;
+    }
+
+    @ToString
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @RequiredArgsConstructor
+    @ApiModel(description = "The request details supplied when updating a prescriptions info.")
+    private static final class UpdateRequestPayload{
+        @ApiModelProperty(value = "The prescriptions id",required = true)
+        final Long id;
+        @ApiModelProperty(value = "The prescriptions name.")
+        final String name;
+        @ApiModelProperty(value = "The prescriptions amount.")
+        final Long amount;
+    }
+
+    @ToString
+    @Getter
+    @RequiredArgsConstructor
+    @ApiModel(description = "The request details supplied when deleting a prescription from the database.")
+    private static final class DeleteRequestPayload{
+        @ApiModelProperty(
+                value = "The unique, database identifier for the prescription to retrieve" +
+                        " If null, return all prescriptions in the database.",
+                required = true,
+                example = "1024"
+        )
+        final Long id;
+    }
+
+    private static final class PrescriptionResponsePayload{
+        public PrescriptionResponsePayload(final Prescription prescription){
+            id = prescription.getId();
+            name = prescription.getName();
+            amount = prescription.getAmount();
+            patient = prescription.getPatient();
+            doctor = prescription.getDoctor();
+        }
+
+        final Long id;
+        final String name;
+        final Long amount;
+        final Patient patient;
+        final Doctor doctor;
+
+    }
+
     private final PrescriptionService service;
-
-    @Autowired
-    public PrescriptionController(PrescriptionService service) {
-        this.service = service;
-    }
-
-    /**
-     * Allow a user to get a list of all prescriptions in the database.
-     * @return The list of all prescriptions.
-     */
-    @GetMapping(path = "getPrescriptions")
-    public List<Prescription> getPrescriptions(){
-        //make the returned collection unmodifiable
-        return Collections.unmodifiableList(service.getPrescriptions());
-    }
-
-    /**
-     * Allow a user to get a single prescription from the database.
-     * @param presId The prescriptions id.
-     * @return The prescription.
-     */
-    @GetMapping(path = "getPrescription/{presId}")
-    public Prescription getPrescription(@PathVariable final long presId){
-        return service.getPrescription(presId);
-    }
-
-    /**
-     * Allow a user to add a method to the database.
-     * @param presId The prescriptions id.
-     */
-    @PostMapping("add")
-    public HttpStatus add(final long presId){
-        return service.add(presId);
-    }
-
-    /**
-     * Allow a user to delete a prescription from the repository.
-     * @param presId The prescriptions id.
-     */
-    @DeleteMapping("delete/{presId}")
-    public HttpStatus delete(@PathVariable final long presId){
-        return service.delete(presId);
-    }
-
-    /**
-     * Allow a user to update a prescription.
-     * @param presId The prescriptions id.
-     * @param amount The prescriptions new amount.
-     */
-    @PutMapping("update/{presId}/{amount}")
-    public HttpStatus changeAmount(@PathVariable final long presId, @PathVariable final long amount){
-        return service.update(presId,amount);
-    }
 }
